aabc6708ac30e7c1de554d34069514cba72c62fa
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-12-12 15:50:19 +0000

    Function, loop, dictionary and condition

---
+++ financial_control.py
@@ -0,0 +1,17 @@
+# 2. Controle Financeiro
+
+# Faça um sistema que:
+
+# Armazene despesas em um dicionário no formato {categoria: valor}.
+
+# Tenha função para adicionar despesas.
+
+# Use loop para permitir várias inserções.
+
+# Ao final, calcule e mostre:
+
+# total gasto
+
+# maior despesa
+
+# categoria mais usada
